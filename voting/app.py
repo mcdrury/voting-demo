2c50993b9db2de1237e9f037dba87384b66f977b
Ubuntu <ubuntu@node0-75d.0y1sxblpffhexe3l2cfegj303e.jx.internal.cloudapp.net>
2017-04-19 21:39:10 +0000

    Changing the voting options

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Football"
+optionB = "CrossCountry"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
